5bd413db1f45d3df0cbcea863940aa585283a7b6
Emil <emilfrankiewicz2@gmail.com>
2021-06-01 13:05:53 +0200

    First commit.

---
+++ src/test/java/pl/emilfrankiewicz/PosterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.emilfrankiewicz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
